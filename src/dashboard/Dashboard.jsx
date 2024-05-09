f431860168c0fdd18129c6be108ee3b9ff094a4e
AmshShrestha <sthaamsh37@gmail.com>
2024-05-09 16:05:17 +0545

    Dashboard icon

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div></div>
+    </div>
+  );
 };
 
 export default Dashboard;
